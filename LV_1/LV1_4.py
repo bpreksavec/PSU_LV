580bd27c2ce1b8544215144a4be6411b2c397dac
Benjamin <95294122+bpreksavec@users.noreply.github.com>
2024-04-02 18:59:55 +0200

    Add files via upload

---
+++ LV_1/LV1_4.py
@@ -0,0 +1,23 @@
+def average(dat_name):
+    try:
+        with open(dat_name, 'r') as file:
+            ukupno = 0
+            br_linija = 0
+            for linija in file:
+                if linija.startswith("X-DSPAM-Confidence:"):
+                    try:
+                        pouzdanost = float(linija.split(":")[1].strip())
+                        ukupno += pouzdanost
+                        br_linija += 1
+                    except ValueError:
+                        continue
+            if br_linija > 0:
+                sred_pouzdanost = ukupno / br_linija
+                print(f"Average X DSPAM-Confidence: {sred_pouzdanost}")
+            else:
+                print("Nema linija s pouzdanoscu.")
+    except FileNotFoundError:
+        print("Datoteka nije pronadjena.")
+
+dat_name = input("Ime datoteke: ")
+average(dat_name)        
